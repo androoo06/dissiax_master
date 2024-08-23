0de253615f75fe6ef00e2da48df6df8f0ecdff62
Andr3wM1ll3r <an873997@ucf.edu>
2024-08-23 16:32:23 -0400

    refactor

--- data.js
+++ data.js
@@ -1,35 +1,96 @@
-// TABULAR DATA
+// text that appears in the paragraph boxes
+let paragraphText = {
+    /// WHAT IS DISSIAX?
+    "about": {
+        /*
+        headerText: [pgText, pgText, ..., pgText]
+        */
 
-let tabs = [
-    "What is Dissiax?",
-    "Why Dissiax?",
-    "Services",
-    "Contact Us",
-]
-let tabLefts = [0, -100, -200, -300]
+        "What is Dissiax?": [
+            "Dissiax Inc. is a corporation focused on providing unbeatable computing quality. We recognized that if reinventing the wheel makes a better wheel, the progress is worth the cost. Our team will improve your experience and ensure that your data is safe."
+        ],
+        
+        "History of Dissiax": [
+            "Dissiax started with a question: why is it possible to hack a computer?",
+            "We started creating server software to try to prevent cyber security attacks, and we found more systems that had problems. Our scope broadened to improving all these systems, but there were restrictions and issues we found within operating systems, not just the applications. Then we started over and decided to rebuild our software from the ground up. Our thought was that if you need to keep patching holes, maybe we should rebuild the base stronger.",
+            "We had years of issues, starting over from scratch many times, and learning. Once we finally had a server that could handle clients better than anything else we've found, we created the corporation Dissiax Inc.",
+            "Since then, we've continued to expand our servers' capabilities, create and improve applications, and improve anything else we can find."
+        ],
 
-// navbar bg & header1 text colors
-let headerColors = {
-    "What is Dissiax?":"rgb(48, 141, 255)",
-    "Why Dissiax?":"rgb(224, 81, 70)",
-    "Services":"rgb(255, 202, 97)",
-    "Contact Us":"rgb(98, 195, 0)",
-}
+        "Mission, Vision, & Values": [
+            "$SUBHEADER$Our Mission:",
+            "To perfect software",
+            "$SUBHEADER$Our Vision:",
+            "A world where no one worries about cyber security, all computers run perfectly optimized without waste, and all applications are intuitive and easy to use",
+            "$SUBHEADER$Our Values:",
+            `
+                &emsp14;&#149;&emsp14;&emsp14;Forthright honesty<br>
+                &emsp14;&#149;&emsp14;&emsp14;Constructive criticism
+            `
+        ]
+    },
 
-// paragraph background colors
-let paragraphColors = {
-    "What is Dissiax?":"rgb(211, 224, 255)",
-    "Why Dissiax?":"rgb(224, 181, 170)",
-    "Services":"rgb(255, 241, 170)",
-    "Contact Us":"rgb(202, 255, 191)",
-}
+    /// WHY DISSIAX?
+    "why": {
+        "Who We Help" : [
+            "$SUBHEADER$We help organizations who:",
+            `
+                &emsp14;&#149;&emsp14;&emsp14;Have sensitive information to protect<br>
+                &emsp14;&#149;&emsp14;&emsp14;Want access to their data anywhere with internet<br>
+                &emsp14;&#149;&emsp14;&emsp14;Want access to their data restricted to certain areas or devices<br>
+                &emsp14;&#149;&emsp14;&emsp14;Don't want to worry about losing their data or having it used against them
+            `
+        ],
 
-// paragraph text colors
-let paragraphTextColors = {
-    //"What is Dissiax?":"rgb(48, 141, 255)",
-    //"Why Dissiax?":"rgb(224, 81, 70)",
-    //"Services":"rgb(255, 202, 97)",
-    //"Contact Us":"rgb(98, 195, 0)",
+        "Why Dissiax?" : [
+            "$SUBHEADER$Our servers will keep your information safe and secure.",
+            "We designed and tested every peice of software running in our servers over years to ensure their reliability and security. No matter what anyone sends at our servers, you will stay safe.",
+            "$SUBHEADER$We provide you with applications that are intuitive and easy to use.",
+            "If anything doesn't make sense or is unsatisfactory, we will gladly make changes to your liking and even fully redesign applications until you're happy"
+        ],
+    },
 
-    "__Default": "rgb(0, 0, 0)", // used if entry for any of the above isn't found
+    /// SERVICES
+    "serv": {
+        "Services" : [
+            "We provide secure servers to handle data storage and easy to use applications for connecting to those servers",
+            "$SUBHEADER$Examples of our Services:",
+            `
+                &emsp14;1. &emsp14;&emsp14;Storing records<br>
+                &emsp14;2. &emsp14;&emsp14;Hosting file servers<br>
+                &emsp14;3. &emsp14;&emsp14;Storing proprietary and sensitive data<br>
+                &emsp14;4. &emsp14;&emsp14;Hosting websites
+            `
+        ],
+        "Service Methods": [
+            "$SUBHEADER$Methods of Service:",
+            `
+                &emsp14;&#149;&emsp14;&emsp14;We host a server that you connect to<br>
+                &emsp14;&#149;&emsp14;&emsp14;We install a server at your facility that you interact with
+            `,
+            "We would love to hear from you if you have any questions or custom requests"
+        ]
+    },
+
+    /// CONTACT
+    "cont": {
+        "Contact" : [
+            "Let us know if you have any questions or requests; we look forward to hearing from you!",
+            `
+                <b>Email:</b> &thinsp; <a onClick="javascript:window.open('mailto:dissiaxinc@gmail.com', 'mail');event.preventDefault()" href="mailto:dissiaxinc@gmail.com">dissiaxinc@gmail.com</a><br>    
+            `,
+            `
+                <b>Phone:</b> &thinsp; <a href="tel:612-532-2487">(612) 532-2487</a>
+            `
+        ],
+        "Meet our staff" : [],
+        "$CAROUSEL$carousel-1": [
+            // [title, innerHTML]
+            ['Brenden', ''], 
+            ['Brenden', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae cumque quia dolor, voluptatibus, veritatis illo expedita sequi, sit numquam reprehenderit dolores! Quos eveniet quia, voluptatum quae sapiente sequi quo aliquam!'],
+            ['Andrew', '<img id=andrew alt="Andrew Miller">'],
+            ['Andrew', ''], 
+            ['Jacob', ''],
+        ],
+    },
 }
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let currentTab = 0
+let mobileWidth = 600;
+let currentTab = null
+let currentItems = null
 let animating = false
 let carouselMoving = false
-
-//carouselHTMLs[title[0]] = paragraphs
 
 let observer = new IntersectionObserver((entries) => {
     entries.forEach((object) => {
@@ -17,56 +17,187 @@ function wrap(index, max) {
     return index
 }
 
-function scrollerEffects(scroller, bool) {
-    scroller.style.opacity = bool ? 1 : 0
+/// Responsiveness ///
+
+function switchToMobile() {
+    let style = getComputedStyle(document.body);
+    let bgColor = style.getPropertyValue(`--header-${currentTab}`)
+
+    // $(".header").css("background-color", bgColor)
 }
 
-function getTab(id) {
-    return parseInt(id.split("#")[1])
+function switchToPC() {
+    let style = getComputedStyle(document.body);
+    let bgColor = style.getPropertyValue(`--pg-${currentTab}`)
+
+    // $(".header").css("background-color", bgColor)
 }
 
-function swipe(newTab, oldTab) {
-    // slide all the tabs over left/right
-    let steps = Math.abs(newTab - oldTab)
-    let dir = Math.sign(newTab - oldTab)
-    let len = tabs.length
+function displayWindowSize() {
+    let w = document.documentElement.clientWidth;
 
-    // debounce to stop from possible jitterclicking
-    animating = true
-    setTimeout(() => {
-        animating = false
-    }, 502 * steps)
+    if (w < mobileWidth) {
+        switchToMobile()
+    } else {
+        switchToPC()
+    }
+}
 
-    // navbar color
-    let navbar = document.getElementById("navbar")
-    navbar.style.backgroundColor = headerColors[tabs[newTab]] || "var(--light)"
+/// Site ///
 
-    for (let i=0; i<tabs.length; i++) {
-        document.getElementById(tabs[i]).classList.add("invisibleScroll")
+function generateCarousel(carouselId, carouselData) {
+    let slides = ""
+    for (let i=0; i<carouselData.length; i++) {
+        let data = carouselData[i]
+        let left = (i == 0) ? "0%" : "-100%";
+        let str = `
+            <div style="left: ${left};"">
+                <h3 style="background-color: transparent;">${data[0]}</h3>
+                <div>
+                    ${data[1]}
+                </div>
+            </div>
+        `
+        slides += str
     }
 
-    // shift all elements over left/right 100% to align with what the user clicked
-    for (let i=0; i<steps; i++) { // steps to get to desired
-        for (let j=0; j<len; j++) { // applying the step to each element(category)
+    var xmlString = `
+        <div class="carousel carousel-color scroller" style="left: -77.5%;">
+            <button id="c1-lb" class="left-btn" style="left: 0;">〈</button>
+            <div id="${carouselId}" data-currentvisible="0">
+                ${slides}
+            </div>
             
-            tabLefts[j] += (dir * 100)
-            
-            let percent = `${tabLefts[j]}%`
-            let element = document.getElementById(tabs[j])
+            <button id="c1-rb" class="right-btn" style="right: 0;">〉</button>
+        </div>
+    `;
+    
+    const parser = new DOMParser();
+    let document = parser.parseFromString(xmlString, "text/html");
 
-            element.style.left = percent
+    let carousel = document.body.children[0]
+    return carousel
+}
+
+// preload tab outside of screen, and fade it in
+function loadTab(tabName) {
+    let padElement = document.createElement("div")
+    $(padElement).addClass("pad1")
+    
+    let dummyContent = document.createElement("div")
+    dummyContent.id = "dummyContent"
+    dummyContent.appendChild(padElement)
+
+    let items = [dummyContent]
+    
+    // load dummy items in
+    for (const [header, paragraphTexts] of Object.entries(paragraphText[tabName])) {
+        if (header.includes("$CAROUSEL$")) {
+            let newHeader = header.split("$CAROUSEL$")[1]
+            let carousel = generateCarousel(newHeader, paragraphTexts)
+            dummyContent.appendChild(carousel)
+            dummyContent.appendChild(padElement.cloneNode(true))
+            items.push(carousel)
+            continue;
         }
+
+        let headerEl = document.createElement("div")
+        $(headerEl).css("left", "137.5%")
+        $(headerEl).css("opacity", "0")
+        $(headerEl).addClass("header content-box scroller");
+        $(headerEl)[0].innerHTML = header
+
+        dummyContent.appendChild(headerEl)
+        dummyContent.appendChild(padElement.cloneNode(true))
+        items.push(headerEl)
+
+        paragraphTexts.forEach(txt => {
+            let pgEl = document.createElement("div")
+            $(pgEl).css("left", "-87.5%")
+            $(pgEl).css("opacity", "0")
+            $(pgEl).addClass("paragraph content-box scroller")
+
+            let htmlTxt = txt
+            if (htmlTxt.includes("$SUBHEADER$")) {
+                htmlTxt = txt.split("$SUBHEADER$")[1]
+                $(pgEl).addClass("subheader")
+            }
+            $(pgEl)[0].innerHTML = htmlTxt
+
+            dummyContent.appendChild(pgEl)
+            dummyContent.appendChild(padElement.cloneNode(true))
+            items.push(pgEl)
+        })
     }
 
-    setTimeout(() => {
-        for (let i=0; i<tabs.length; i++) {
-            document.getElementById(tabs[i]).classList.remove("invisibleScroll")
+    for (let i=0; i<2; i++) {
+        dummyContent.appendChild(padElement.cloneNode(true))
+    }
+
+    let mobilePadElement = document.createElement("div")
+    $(mobilePadElement).addClass("mobilePad")
+    dummyContent.appendChild(mobilePadElement)
+
+    return items
+}
+
+function fade(list, out) {
+    for (i=1; i<list.length; i++) {
+        if ($(list[i]).hasClass("paragraph")) {
+            list[i].style.left = (out) ? "-87.5%" : "12.5%"
+        } else if ($(list[i]).hasClass("header")) {
+            list[i].style.left = (out) ? "137.5%" : "37.5%"
+        } else if ($(list[i]).hasClass("carousel")) {
+            list[i].style.left = (out) ? "-77.5%" : "22.5%"
         }
-    }, 495)
+        list[i].style.opacity = (out) ? 0 : 1
+    }
+}
 
-    currentTab = newTab
+function switchTabs(x) {
+    let oldItems = currentItems
+    let oldTab = currentTab
+    currentTab = $(x.target).data("tabname")
 
-    document.title = tabs[newTab] + " - Dissiax"
+    if (oldTab == currentTab) return;
+
+    let items = loadTab(currentTab)
+    currentItems = items
+
+    let dummyContent = items[0]
+    let oldContent = $("#content")[0]
+
+    oldContent.id = "dying"
+    oldContent.style.position = "absolute"
+    oldContent.style.overflow = "hidden"
+    oldContent.style.height = "100%";
+    
+    dummyContent.id = "content"
+    document.body.appendChild(dummyContent)
+
+    if (oldItems !== null) {
+        fade(oldItems, true)
+    }
+
+    setTimeout(function () {
+        fade(items, false)
+    }, 0)
+
+    setTimeout(function () {
+        document.body.removeChild(oldContent)
+        // dummyContent.style.position = "relative"
+    }, 2500)
+
+    let style = getComputedStyle(document.body);
+    let navColor = style.getPropertyValue(`--header-${currentTab}`)
+    let pgColor = style.getPropertyValue(`--pg-${currentTab}`)
+
+    $("#navbar").css("background-color", navColor)
+    $(".content-box").css("background-color", pgColor)
+    $(".carousel-color").css("background-color", pgColor)
+
+    loadParticles(pgColor)
+    onScroll()
 }
 
 function slideCarousel(carouselId, direction) {
@@ -103,29 +234,179 @@ function slideCarousel(carouselId, direction) {
     carousel.dataset.currentvisible = String(nextIndex)
 }
 
-window.onload = function () {
-    // preload()
+function scrollerEffects(scroller, bool) {
+    scroller.style.opacity = bool ? 1 : 0
+}
 
-    for (let id=0; id<tabs.length; id++) {
-        let child = document.getElementById(`tab#${id}`)
-        child.onclick = function() {
-            let tab = getTab(child.id)
-            if (currentTab == tab) return;
-            if (animating) return;
+function rgbToHex(color) {
+    var a = color.split("(")[1].split(")")[0];
+    a = a.split(",");
+    var b = a.map(function(x){             //For each array element
+        x = parseInt(x).toString(16);      //Convert to a base16 string
+        return (x.length==1) ? "0"+x : x;  //Add zero if we get only one character
+    })
+    b = "#"+b.join("");
+    return b
+}
 
-            // change tabs
-            swipe(tab, currentTab)
+function loadParticles(colorRGB) {
+    let color = rgbToHex(colorRGB)
+    console.log(color)
+    particlesJS("particles-js", {
+            "particles": {
+                "number": {
+                    "value": 20,
+                    "density": {
+                        "enable": false,
+                        "value_area": 1000
+                    }
+                },
+                "color": {
+                    "value": color
+                },
+                "shape": {
+                    "type": "polygon",
+                    "stroke": {
+                        "width": 0,
+                        "color": color
+                    },
+                    "polygon": {
+                        "nb_sides": 6
+                    },
+                    "image": {
+                        "src": "assets/Dissiax_Logo.png",
+                        "width": 100,
+                        "height": 100
+                    }
+                },
+                "opacity": {
+                    "value": 1,
+                    "random": false,
+                    "anim": {
+                        "enable": false,
+                        "speed": 1,
+                        "opacity_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "size": {
+                    "value": 3,
+                    "random": false,
+                    "anim": {
+                        "enable": false,
+                        "speed": 40,
+                        "size_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "line_linked": {
+                    "enable": true,
+                    "distance": 150,
+                    "color": color,
+                    "opacity": 0.75,
+                    "width": 2
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 2,
+                    "direction": "none",
+                    "random": true,
+                    "straight": false,
+                    "out_mode": "out",
+                    "attract": {
+                        "enable": true,
+                        "rotateX": 600,
+                        "rotateY": 1200
+                    }
+                }
+            },
+            "interactivity": {
+                "detect_on": "window",
+                "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": "bubble"
+                    },
+                    "onclick": {
+                        "enable": false,
+                        "mode": "grab"
+                    },
+                    "resize": true
+                },
+                "modes": {
+                    "grab": {
+                        "distance": 200,
+                        "line_linked": {
+                            "opacity": 1
+                        }
+                    },
+                    "bubble": {
+                        "distance": 100,
+                        "size": 15,
+                        "duration": 0.25,
+                        "opacity": 1,
+                        "speed": 1
+                    },
+                    "repulse": {
+                        "distance": 100
+                    },
+                    "push": {
+                        "particles_nb": 10
+                    },
+                    "remove": {
+                        "particles_nb": 2
+                    }
+                }
+            },
+            "retina_detect": true
         }
+    );
+}
+
+function onScroll(x) {
+    let style = getComputedStyle(document.body);
+    let maxScroll = Math.ceil($("#content")[0].scrollHeight - $("#content").innerHeight())
+        
+    if (maxScroll > 0) {
+        maxScroll = Math.max(25, maxScroll)
     }
+    
+    let gradientPercent = Math.min(1, $(document.body).scrollTop() / maxScroll) * 48
+    let otherPercent = 100 - gradientPercent
+
+    // const fromPercent = (from,to,current) => ((to - from) * current/100) + from;
+    // let p = Math.floor(gradientPercent * 360)
+    
+    let navColor = style.getPropertyValue(`--header-${currentTab}`)
+    console.log(navColor)
+
+    let grad = `linear-gradient(white ${Math.max(45, otherPercent)}%, ${navColor} ${gradientPercent}%)`
+    $("body").css("background",grad)
+}
+
+$(function () {
+    $(".tab-switch").on("click", switchTabs)
+
+    // displayWindowSize();
+    // window.addEventListener("resize", displayWindowSize);
+
+    let aboutUs = {target: $("#tab-about")[0].children[0]}
+    switchTabs(aboutUs)
+
+    $(document.body).on("click", "#c1-rb", function() {
+        slideCarousel("carousel-1", 1)
+    })
+
+    $(document.body).on("click", "#c1-lb", function() {
+        slideCarousel("carousel-1", -1)
+    })
+
+    let style = getComputedStyle(document.body);
+    $(document.body).on('scroll', onScroll)
 
     let scrollers = document.querySelectorAll(".scroller")
     scrollers.forEach((element) => observer.observe(element))
 
-    document.getElementById("c1-rb").onclick = function() {
-        slideCarousel("carousel-1", 1)
-    }
-    
-    document.getElementById("c1-lb").onclick = function() {
-        slideCarousel("carousel-1", -1)
-    }
-}
+    let pgColor = style.getPropertyValue(`--pg-${currentTab}`)
+    loadParticles(pgColor)
+})
